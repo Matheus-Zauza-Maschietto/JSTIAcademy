0cfa82bfc588d60788e63583cdb6eb8f9bb19ae3
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2022-10-14 13:34:42 -0300

    update

---
+++ Desafios/projeto1/script/autentication.js
@@ -0,0 +1,29 @@
+
+export function autenticator(){
+    const login = document.querySelector("#login")
+    const usuario = document.querySelector('#usuario')
+    const senha = document.querySelector("#senha")
+    let autenticado = false
+    let users = [
+        {"user": "matheus", "senha": "12345"},
+        {"user": "andre", "senha": "666"},
+        {"user": "miriam", "senha": "12345"}
+    ]
+    
+    login.addEventListener('click', function(){
+        
+        let UserInput = usuario.value
+        let passwordInput = senha.value
+
+        autenticado = false
+        for(let item of users){
+            if(item["user"] == UserInput.trim() && item["senha"] == passwordInput.trim()){
+                autenticado = true 
+                window.location.href = `./panel.html`
+            }
+        }
+        if(autenticado === false){
+            alert("Usuário ou senha errados")
+        }
+    })
+}
--- Desafios/projeto1/script/index.js
+++ Desafios/projeto1/script/index.js
@@ -1,25 +1,4 @@
+import {autenticator} from './autentication.js'
 window.onload = function(){
-    const login = document.querySelector("#login")
-    const usuario = document.querySelector('#usuario')
-    const senha = document.querySelector("#senha")
-    let autenticado = false
-    let users = [
-        {"user": "matheus", "senha": "12345"},
-        {"user": "andre", "senha": "666"},
-        {"user": "miriam", "senha": "12345"}
-    ]
-
-    login.addEventListener('click', function(){
-        autenticado = false
-        for(item of users){
-            if(item["user"] == usuario.value && item["senha"] == senha.value){
-                autenticado = true 
-                   
-            }
-        }
-        if(autenticado === false){
-            alert("Usuário ou senha errados")
-        }
-    })
-
+    autenticator()
 }
